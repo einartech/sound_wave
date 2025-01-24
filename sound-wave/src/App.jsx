4a3349e1e257d50d466788dbc33ec87cba3b0775
einartech <imalovelyghost13@gmail.com>
2025-01-24 14:33:38 +0100

    component/ img reusable component created, called into appjs, must be change this call to main when that component is ready

--- sound-wave/src/App.jsx
+++ sound-wave/src/App.jsx
@@ -1,5 +1,11 @@
 import Router from "./components/Router/Router";
 //Importar Componentes Header, Main y Footer aqui
+import Image from "./components/Image/Image";
+
+//landingPageGirl - Mover a Main cuando este terminado
+import landingPageGirl from "./assets/images/landing-page-girl.png";
+
+import covers from "./assets/images/covers.jpg";
 
 import "./App.css";
 
@@ -8,6 +14,20 @@ export default function App() {
     //Llamar Componentes Header, Main y Footer aqui
     <>
       <h1>Component APP</h1>
+      <h3>A continuacion inician los componentes REUTILIZABLES de main</h3>
+      <Image
+        img={landingPageGirl}
+        alt="Una chica de pie con auriculares en la página de aterrizaje"
+        width="300"
+        height="auto"
+      />
+      <Image
+        img={covers}
+        alt="Varias portadas de discos englobadas en un rectangulo negro"
+        width="300"
+        height="auto"
+      />
+      <h3>A continuacion finalizan los componentes REUTILIZABLES de main</h3>
     </>
   );
 }
