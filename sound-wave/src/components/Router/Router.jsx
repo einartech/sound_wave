eb92ed74cfff8c72218e89708007c5ae9d4e6b1a
einartech <imalovelyghost13@gmail.com>
2025-01-24 10:27:53 +0100

    component/ Router created and moduleccs added

---
+++ sound-wave/src/components/Router/Router.jsx
@@ -0,0 +1,11 @@
+import "./RouterStyle.css";
+import RouterStyle from "./RouterStyle.module.css";
+
+export default function Router() {
+  return (
+    <>
+      <h1 className={RouterStyle.red}>Componente Router</h1>
+      <h1 className={RouterStyle.blue}>Componente Router</h1>
+    </>
+  );
+}
