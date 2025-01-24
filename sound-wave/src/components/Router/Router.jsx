2aa447ebeb0c82f21e4ac332c25457ff1753e4b5
einartech <imalovelyghost13@gmail.com>
2025-01-24 10:41:02 +0100

    modulecss/ improve naming of files

--- sound-wave/src/components/Router/Router.jsx
+++ sound-wave/src/components/Router/Router.jsx
@@ -1,11 +1,10 @@
-import "./RouterStyle.css";
-import RouterStyle from "./RouterStyle.module.css";
+import style from "./Style.module.css";
 
 export default function Router() {
   return (
     <>
-      <h1 className={RouterStyle.red}>Componente Router</h1>
-      <h1 className={RouterStyle.blue}>Componente Router</h1>
+      <h1 className={style.red}>Componente Router</h1>
+      <h1 className={style.blue}>Componente Router</h1>
     </>
   );
 }
