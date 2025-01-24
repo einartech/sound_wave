cefda0b84dac8c2aca6db2a9c6e41ca30dd50a43
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-01-24 18:54:39 +0100

    modulecss/wip

--- sound-wave/src/components/Footer/Footer.jsx
+++ sound-wave/src/components/Footer/Footer.jsx
@@ -5,12 +5,16 @@ import facebookImg from '../../assets/images/facebook.svg'
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
+        <div className={styles.footerLeftPart}>
         <p className={styles.footerFont}>About Us</p>
         <p className={styles.footerFont}>Contact</p>
+        </div>
+        <div className={styles.footerRightPart}>
         <img className={styles.footerImg} src={twitterImg}/>
         <p className={styles.footerSocials}>Twitter</p>
         <img className={styles.footerImg} src={facebookImg}/>
         <p className={styles.footerSocials}>Facebook</p>
+        </div>
     </footer>
   )
 }
