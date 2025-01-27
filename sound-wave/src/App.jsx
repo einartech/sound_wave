eec3d3d0efc7e3636a91d436d916d186dab406ff
einartech <imalovelyghost13@gmail.com>
2025-01-27 16:04:31 +0100

    components/all secondary components of main added

--- sound-wave/src/App.jsx
+++ sound-wave/src/App.jsx
@@ -1,46 +1,16 @@
-import React from 'react';
+import React from "react";
 
-//import Router from './components/Router/Router';
-//Importar Componentes Header, Main y Footer aqui
-import Image from "./components/Image/Image";
-import Button from "./components/Button/Button";
-import Title from './components/Title/Title';
 import Navbar from "./components/navbar/Navbar";
-
-//landingPageGirl & covers - Mover a Main cuando este terminado
-import landingPageGirl from "./assets/images/landing-page-girl.png";
-import covers from "./assets/images/covers.jpg";
-import Main from './components/main/Main';
-//import "./App.css";
+import Main from "./components/main/Main";
 import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     //Llamar Componentes Header, Main y Footer aqui
     <>
-      <Footer/>
-
       <Navbar />
       <Main />
-      <Image
-        img={landingPageGirl}
-        alt="Una chica de pie con auriculares en la página de aterrizaje"
-        width="300"
-        height="auto"
-      />
-      <Image
-        img={covers}
-        alt="Varias portadas de discos englobadas en un rectangulo negro"
-        width="300"
-        height="auto"
-      />
-      <Button type="button" value="Boton 1" />
-      <Button type="button" value="Boton 2" />
-      <Title text ="main title"/>
-      <Title text ="title 2"/>
-      <Title text ="title 3"/>
-
-      <h3>A continuacion finalizan los componentes REUTILIZABLES de main</h3>
+      <Footer />
     </>
   );
 }
--- sound-wave/src/components/main/Main.jsx
+++ sound-wave/src/components/main/Main.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import style from "./Main.module.css";
 
+import Button from "../Button/Button";
+import Title from "../Title/Title";
+import Image from "../Image/Image";
+
+//landingPageGirl & covers - Mover a Main cuando este terminado
+import landingPageGirl from "../../assets/images/landing-page-girl.png";
+import covers from "../../assets/images/covers.jpg";
+
 function Main() {
   return (
     <div className={style["main-container"]}>
       <section className={style["left-section"]}>
-        <h2>Sección Izquierda</h2>
-        <p>Contenido de la seccion izquierda</p>
+        <Title text="main title" />
+        <p>parrafo descriptivo</p>
+
+        <Button type="button" value="boton chica" />
       </section>
 
       <section className={style["right-section"]}>
-        <h2>Seccion Derecha</h2>
-        <p>Contenido de la sección derecha</p>
+        <Image
+          img={landingPageGirl}
+          alt="Una chica de pie con auriculares en la página de aterrizaje"
+          width="50vh"
+          height="50vh"
+        />
       </section>
     </div>
   );
