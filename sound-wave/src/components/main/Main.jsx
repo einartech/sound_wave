8ef77d7b51ec8d5189e4a0ffc00eb9b434aadac5
einartech <imalovelyghost13@gmail.com>
2025-01-28 15:43:59 +0100

    order/ cleaning main component

--- sound-wave/src/components/main/Main.jsx
+++ sound-wave/src/components/main/Main.jsx
@@ -1,42 +1,16 @@
 import React from "react";
 import style from "./Main.module.css";
 import Icons from "../Icons/Icons";
-import iconsImage from "../../assets/images/microphone.svg"
-import moreIcon from "../../assets/images/more.svg"
-import AlbumsIcon from "../../assets/images/albums.svg"
+import iconsImage from "../../assets/images/microphone.svg";
+import moreIcon from "../../assets/images/more.svg";
+import AlbumsIcon from "../../assets/images/albums.svg";
 
 function Main() {
   return (
     <div className={style["main-container"]}>
-      <section className={style["left-section"]}>
-        <h2>Sección Izquierda</h2>
-        <p>Contenido de la seccion izquierda</p>
-      </section>
+      <section className={style["left-section"]}></section>
 
-      <section className={style["right-section"]}>
-        <Icons
-        img={iconsImage}
-        text= "Charts"
-        alt= "microphone icon"
-        width= "100px"
-        height= "100px"
-        />
-         <Icons
-        img={AlbumsIcon}
-        text= "Albums"
-        alt= "Albums icon"
-        width= "100px"
-        height= "100px"
-        />
-          <Icons
-        img={moreIcon}
-        text= "More"
-        alt= "more icon"
-        width= "100px"
-        height= "100px"
-        />
-       
-      </section>
+      <section className={style["right-section"]}></section>
     </div>
   );
 }
