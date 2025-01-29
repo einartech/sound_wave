1a3ceda6ff510697d3328bfe1fb376af50e34f78
SiGu <miskybox@gmail.com>
2025-01-29 10:10:49 +0100

    changes:general css-mockup-page1

--- sound-wave/src/App.jsx
+++ sound-wave/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/header/Header";
 import Main from "./components/main/Main";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Footer />
       <Main />
+      <Footer />
     </>
   );
 }
