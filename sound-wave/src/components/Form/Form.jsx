2ae35a06df26854976c75aa712c2d354e357b75a
RieraDiPe <rieradipe@gmail.com>
2025-01-31 11:11:56 +0100

    Scrum-37 merge development

--- sound-wave/src/components/Form/Form.jsx
+++ sound-wave/src/components/Form/Form.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const FormContainer = styled.div`
-background-color: red;
-width: 40%;
-heigth: 40%;
+display: flex;
+background-color: #202027;
+width: 55%;
+heigth: 25%;
 padding: 20px;
 border-radius: 5px;
 box-shadow: 0 0 10px rgba(0, 0, 0, 1);
@@ -16,15 +17,21 @@ font-weight: bold;
 `;
 
 const Input = styled.input`
+background-color: #202027;
 width: 100%;
 padding: 10px;
 margin-bottom: 15px;
-border: 1px solid #ddd;
+border: 1px solid #2E3852;
 border-radius: 4px;
+&:focus {
+border: 1px solid ##596da0
+}
 `;
 
+
 const SubmitButton = styled.button`
-background-color: #007bff;
+background-color: #1762A7;
+width: 100%;
 color: white;
 padding: 10px 20px;
 border: none;
@@ -33,7 +40,10 @@ cursor: pointer;
 transition: background-color 0.3s ease;
 
 &:hover {
-background-color: #0056b3
+background-color: #2180d8;
+}
+&:autofocus {
+background-color: #2796ff;
 }
 `;
 
@@ -51,12 +61,12 @@ const Form = () => {
                 <Input type="text" id="name" name="name" required />
 
                 <Label htmlFor="email">Email:</Label>
-                <Input type="email" id="email" name="email" required/>
+                <Input type="email" id="email" name="email" required />
 
                 <Label htmlFor="password">Password:</Label>
-                <Input type="password" id="password" name="password" required/>
+                <Input type="password" id="password" name="password" required />
 
-            <SubmitButton type="submit">Join Now</SubmitButton>
+            <SubmitButton type="submit" autoFocus>Join Now</SubmitButton>
             </form>
             </FormContainer>
     </>
