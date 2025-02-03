a9896a8ab9bf64651cd944ccdaac16a124c3abec
SiGu <miskybox@gmail.com>
2025-02-03 16:54:38 +0100

    Changes: CSS-responsive-Join

--- sound-wave/src/components/pageJoin/Join.jsx
+++ sound-wave/src/components/pageJoin/Join.jsx
@@ -3,131 +3,6 @@ import styled from "styled-components";
 import Form from "../Form/Form";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-//import Title from "../Title/Title";
-/*
-const MainDiv = styled.section`
-  display: flex;
- /* flex-direction: row;*//*
-  height: 100vh;
-  color: white;
-  overflow: hidden;
-  position: relative;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    text-align: center;
-    height: 100vh;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-const Circle = styled.div`
-  position: absolute;
-  border-radius: 50%;
-  opacity: 0.5;
-`;
-
-const Circle1 = styled(Circle)`
-  width: 550px;
-  height: 550px;
-  background-color: #bc3a801a;
-  left: -165px;
-  bottom: 100px;
-
-  @media (max-width: 768px) {
-    width: 100px;
-    height: 100px;
-    left: 20px;
-  }
-`;
-
-const Circle2 = styled(Circle)`
-  width: 450px;
-  height: 450px;
-  background-color: #3457b21a;
-  top: -210px;
-  left: 300px;
-
-  @media (max-width: 768px) {
-    width: 100px;
-    height: 100px;
-    top: 20px;
-    left: 130px;
-  }
-`;
-
-const LeftSection = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: right;
-  text-align: left;
-  align-items: center;
-  margin: 100px;
-  bottom: 45px;
-`;
-
-const RightSection = styled.div`
-  flex: 1;
-  flex-direction: column;
-  position: relative;
-  top: 250px;
-  justify-content: center;
-  text-align: left;
-
-  @media (max-width: 768px) {
-    flex-direction:row;
-    align-items: flex-bottom;
-    height: 50vh;
-    padding-left: 1em;
-  margin-bottom: 20px;
-  }
-
-  h1 {
-    font-size: 2.5rem;
-    font-weight: 500;
-    margin-bottom: 10px;
-  
-  
-
-    @media (max-width: 768px) {
-    font-size: 2rem;
-
-    }
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-bottom: 20px;
-
-    @media (max-width: 768px) {
-      font-size: 0.9rem;
-    }
-  }
-`;
-
-const Join = () => {
-  return (
-    <>
-     <Header /> 
-      <MainDiv>
-        <Circle1 />
-        <Circle2 />
-        <LeftSection>
-        <Title text1="Join the " text2="Fun." />  
-        </LeftSection>
-        <RightSection>
-          <Form />n
-        </RightSection>
-      </MainDiv>
-      <Footer />
-    </>
-  );
-};
-
-export default Join;
-*/
-
 
 const MainDiv = styled.section`
   display: flex;
@@ -147,21 +22,24 @@ const MainDiv = styled.section`
 
 const StyledTitle = styled.h1`
   font-size: 2.5rem;
-  font-weight: 500;
+  font-weight: bold;
   margin-bottom: 10px;
   white-space: nowrap;
 
   @media (max-width: 768px) {
-    font-size: 2rem;
+    font-size: 3rem;
     white-space: normal;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
+    position: relative;
+    left: -1em;
+    top: -1em;
   }
 `;
 
 const Highlight = styled.span`
-  color: #ff00ff; 
+  color: #BC3A80; 
   
   @media (max-width: 768px) {
     display: block;
@@ -224,7 +102,13 @@ const RightSection = styled.div`
   padding-right: 100px;
 
   @media (max-width: 768px) {
-    padding-right: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    bottom: 10em;
+    width: 100%;
+    padding: 1.2em;
   }
 `;
 
@@ -236,9 +120,8 @@ const Join = () => {
         <Circle1 />
         <Circle2 />
         <LeftSection>
-          {/* <Title text1="Join the " text2="fun." /> */}
           <StyledTitle>
-            Join The <Highlight>Fun</Highlight>
+            Join the <Highlight>fun.</Highlight>
           </StyledTitle>
         </LeftSection>
         <RightSection>
