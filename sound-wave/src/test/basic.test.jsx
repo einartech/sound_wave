c3afe3dd06f742e1a8ea9a7ec684be10c71dd7ef
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-02-04 09:28:33 +0100

    everything is up and running

--- sound-wave/src/test/basic.test.jsx
+++ sound-wave/src/test/basic.test.jsx
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest";
-import { getByRole, render, screen } from "@testing-library/react";
+import { describe, it } from "vitest";
+import { render } from "@testing-library/react";
 import Header from "../components/header/Header";
 import Footer from "../components/Footer/Footer";
 
