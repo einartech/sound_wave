f17aeb44b9c904b200eacc76a7583f1fdbb9a6b7
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-02-04 10:08:22 +0100

    added links and moduled responsive footer

--- sound-wave/src/components/Footer/Footer.jsx
+++ sound-wave/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 import twitterImg from '../../assets/images/twitter.svg'
 import facebookImg from '../../assets/images/facebook.svg'
 
-export default function Footer() {
+/* export default function Footer() {
   return (
     <>
     <footer className={styles.footerContainer}>
@@ -22,5 +22,30 @@ export default function Footer() {
         </div>
     </footer>
     </>
+  )
+} */
+export default function Footer() {
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerLeftPart}>
+        <a href="https://www.canva.com/design/DAGdlUitKyM/aGVhM9xND0u4RLDWtQ6gBQ/view?utm_content=DAGdlUitKyM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h019692a200">
+        <p className={styles.footerFont}>About Us</p></a>
+        <a href="https://www.paginasamarillas.es/">
+        <p className={styles.footerFont}>Contact</p></a>
+      </div>
+      <div className={styles.footerRightPart}>
+      <div className={styles.wrappedSocials}>
+        <a href="https://twitter.com/tuusuario" target="_blank" rel="noopener noreferrer"  
+        style={{ display: "flex", flexWrap: "wrap", alignContent: "center" }}>
+          <img className={styles.footerImg} src={twitterImg}/>
+          <p className={styles.footerSocials}>Twitter</p>
+        </a>
+        <a href="https://facebook.com/tuusuario" target="_blank" rel="noopener noreferrer" style={{ display: "flex", flexWrap: "wrap", alignContent: "center" }}>
+          <img className={styles.footerImg} src={facebookImg}/>
+          <p className={styles.footerSocials}>Facebook</p>
+        </a>
+        </div>
+      </div>
+    </footer>
   )
 }
